dcb3c6dbc2a2b5e6b31c268059b978b2eb388bdd
Ahmad Raza <ahmad.raza@ovada.com>
2026-04-14 08:35:24 +0500

    oauth setup completed

--- src/Signin.js
+++ src/Signin.js
@@ -448,7 +448,7 @@ export default function SignIn(props) {
               color: '#666666 !important',
             }
           }}>
-            Digital World
+            Digital World test oauth
           </Divider>
         </Card>
         
